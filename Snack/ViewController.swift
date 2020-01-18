0c5db72781715bf088b567247facd9aad1400ae1
Rahul Ravi Prakash <rrp.nitdgp@gmail.com>
2020-01-18 14:24:05 +0530

    Initial Commit

---
+++ Snack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Snack
+//
+//  Created by Rahul Ravi Prakash on 18/01/20.
+//  Copyright © 2020 Rahul Ravi Prakash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SnackTests/SnackTests.swift
@@ -0,0 +1,34 @@
+//
+//  SnackTests.swift
+//  SnackTests
+//
+//  Created by Rahul Ravi Prakash on 18/01/20.
+//  Copyright © 2020 Rahul Ravi Prakash. All rights reserved.
+//
+
+import XCTest
+@testable import Snack
+
+class SnackTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
